ee7faecb04e40f237ec6cad3903aaf1bdf6085ff
twumasij <111680745+twumasij@users.noreply.github.com>
2024-11-16 18:49:51 -0500

    stage 2 implementing ping

--- app/main.py
+++ app/main.py
@@ -6,9 +6,16 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
+
+    pong = "+PONG\r\n"
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn , addr =  server_socket.accept() # wait for client
+
+    with conn:
+        conn.recv(1024)
+        conn.send(pong.encond())
+    
 
 
 if __name__ == "__main__":
