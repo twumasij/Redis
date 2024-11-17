42a7e9442fc5a90aa48878f49f18f69298088968
twumasij <111680745+twumasij@users.noreply.github.com>
2024-11-16 19:38:26 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,11 +11,14 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn , addr =  server_socket.accept() # wait for client
+    print(f"accepted connection from {addr}")
 
     with conn:
-        conn.recv(1024)
-        conn.send(pong.encode())
-    
+        while conn:
+
+            conn.recv(1024)
+            conn.send(pong.encode())
+        
 
 
 if __name__ == "__main__":
