c316c5adf0025579aa28ef84daa263d010395616
twumasij <111680745+twumasij@users.noreply.github.com>
2024-11-18 11:25:14 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,23 +1,40 @@
 import socket  # noqa: F401
+import threading
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
 
-    pong = "+PONG\r\n"
+    response = "+PONG\r\n"
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn , addr =  server_socket.accept() # wait for client
-    print(f"accepted connection from {addr}")
+    print("Server started waiting for connections...")
 
+    while True:
+        conn , addr =  server_socket.accept() # wait for client
+
+        print(f"accepted connection - {addr[0]}:{str(addr[1])}")
+        # connect(conn, response)
+        thread = threading.Thread(target = connect, args = [conn , response])
+        thread.start()
+
+
+        
+def connect(conn : socket.socket, response) -> None:
     with conn:
-        while conn:
+        connected: bool = True
+        while connected:
+            data = conn.recv(1024).decode()
+            match data:
+                case "*1\r\n$4\r\nPING\r\n":
+                    response = "+PONG\r\n"
 
-            conn.recv(1024)
-            conn.send(pong.encode())
+            # print(f"received - {data}")
+            # print(f"responding with - {response}")
+            conn.send(response.encode())
         
 
 
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+redis-cli  PING
+redis-cli ping &
+redis-cli ping
