3d7b71b3a301ae69bdc8de2364f544a4d1a64f97
twumasij <111680745+twumasij@users.noreply.github.com>
2024-12-01 15:22:09 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,28 @@ import socket  # noqa: F401
 import threading
 
 
+def process_data(data):
+    data_list = data.strip().split("\r\n")
+    length , *data_list = data_list
+    length = length[1:]
+    length = "".join(length)
+    data_list = data_list[1::2]
+
+
+    # Cheching is the command is echo 
+
+    if data_list[0].lower() == "echo":
+        data_list = data_list[1:]
+
+    data_list = " ".join(data_list)
+
+
+  
+    
+    return length, data_list
+
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
@@ -21,20 +43,6 @@ def main():
         thread = threading.Thread(target = connect, args = [conn , response])
         thread.start()
 
-
-        
-# def connect(conn : socket.socket, response) -> None:
-#     with conn:
-#         connected: bool = True
-#         while conn:
-#             data = conn.recv(1024).decode()
-#             match data:
-#                 case "*1\r\n$4\r\nPING\r\n":
-#                     response = "+PONG\r\n"
-
-#             # print(f"received - {data}")
-#             # print(f"responding with - {response}")
-#             conn.send(response.encode())
         
 
 def connect(conn: socket.socket, response: str) -> None:
@@ -44,12 +52,20 @@ def connect(conn: socket.socket, response: str) -> None:
                 data = conn.recv(1024).decode()
                 if not data:  # Client closed the connection
                     break
-
-                match data:
-                    case "*1\r\n$4\r\nPING\r\n":
-                        response = "+PONG\r\n"
-                
+               
                 print(f"received - {data.encode()}")
+
+                print(data.split())
+                print(process_data(data))
+
+
+                if data.upper() == "*1\r\n$4\r\nPING\r\n":
+                        response = "+PONG\r\n"
+                else:
+                    _,response = process_data(data)
+                    response = "+" + response + "\r\n"
+                
+                
                 print(f"responding with - {response}")
 
                 conn.send(response.encode())
